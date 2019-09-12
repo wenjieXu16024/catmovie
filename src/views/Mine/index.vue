917694feef28ac9be828cabc623b8c49069ecfc6
wenjieXu16024 <wenjie_Xu16024@163.com>
2019-09-12 15:38:55 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Header title="我的"/>
+        <Login/>
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header';
+    import TabBar from '@/components/TabBar';
+    import Login from '@/components/Login';
+    export default {
+        name:'Mine',
+        components:{
+            Header,
+            TabBar,
+            Login
+        }
+    }
+</script>
+
+<style>
+
+</style>
